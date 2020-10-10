f8378bdbe87930b90b7acd6848a4aaf709c43b70
khalid moussaid <khalidmossaid@gmail.com>
2020-10-10 21:33:51 +0100

    update portfolio page

---
+++ component---src-pages-404-js-b0d4432948bc7c24b2a0.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{w2l6:function(n,t,e){"use strict";e.r(t),e.d(t,"default",(function(){return i}));var r=e("dI71"),o=e("q1tI"),u=e.n(o),i=function(n){function t(){return n.apply(this,arguments)||this}return Object(r.a)(t,n),t.prototype.render=function(){return u.a.createElement("div",null,"404 error")},t}(o.Component)}}]);
+//# sourceMappingURL=component---src-pages-404-js-b0d4432948bc7c24b2a0.js.map
