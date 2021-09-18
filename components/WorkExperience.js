f8530c7517bde5818aed0e607d888b7b388eb8ea
kmoussai <khalidmossaid@gmail.com>
2021-09-18 22:04:43 +0100

    add WIP tag

---
+++ components/WorkExperience.js
@@ -0,0 +1,21 @@
+import { RiMenuFill, RiCloseFill, RiGithubFill, RiLinkedinFill, RiTwitterFill, RiHeartFill } from 'react-icons/ri'
+import Card from './Card'
+
+
+
+export default function WorkExperinece({ title }) {
+
+    return (
+        <Card title={title}>
+            
+            <div className="sm:w-2/3 px-3 flex flex-row flex-wrap">
+                <div className="w-full text-right text-gray-700 font-semibold relative pt-3 md:pt-0 flex justify-between flex-col">
+                  
+                  
+                   
+                </div>
+            </div>
+        </Card>
+    )
+
+}
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,28 @@
-
-
-import { RiMenuFill, RiCloseFill, RiGithubFill, RiLinkedinFill, RiTwitterFill, RiHeartFill } from 'react-icons/ri'
+import {
+  RiMenuFill,
+  RiCloseFill,
+  RiGithubFill,
+  RiLinkedinFill,
+  RiTwitterFill,
+  RiHeartFill,
+} from "react-icons/ri";
 import Head from "next/head";
-import { useTheme } from "next-themes"
-import { useEffect, useState } from 'react';
-import Toggle from '../components/Toggle';
-import About from '../components/About';
-import Project from '../components/Project';
-import Technologies from '../components/Technologie';
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+import Toggle from "../components/Toggle";
+import About from "../components/About";
+import Project from "../components/Project";
+import Technologies from "../components/Technologie";
+import WorkExperinece from "../components/WorkExperience";
 
 export default function Home() {
-
   return (
-    <div className='p-2 min-h-full bg-gray-50 dark:bg-gray-900 sm:border-red-600 border md:border-green-600 lg:border-blue-600 relative'>
+    <div className="p-2 min-h-full bg-gray-50 dark:bg-gray-900 sm:border-red-600 border md:border-green-600 lg:border-blue-600 relative">
       <style global jsx>{`
-      #__next {
-        height: 100%;
-      }
-    `}</style>
+        #__next {
+          height: 100%;
+        }
+      `}</style>
       <Head>
         <title>Khalid MOUSSAID</title>
         <link rel="icon" href="/favicon.ico" />
@@ -32,17 +37,24 @@ export default function Home() {
         </div>
       </header>
       <div className="mt-2 mb-2 h-1 bg-gray-600 dark:bg-gray-800" />
+      <div>
+        <div className=" transform bg-blue-500 w-full flex justify-center text-5xl -left-1/4 uppercase">
+          work in progress
+        </div>
+      </div>
       <div className="">
-        <About title="About me"/>
-        <Project title="ProjectS"/>
-        <Technologies title="Technologies"/>
-        
+        <About title="About me" />
+        <Project title="ProjectS" />
+        <WorkExperinece title={"Work experience"} />
+        <Technologies title="Technologies" />
       </div>
 
       <div className="flex items-center justify-center p-2">
-        <p>Made with NextJS (!<RiHeartFill color='red' className='inline' />) by MOUSSAID Khalid</p>
+        <p>
+          Made with NextJS (!
+          <RiHeartFill color="red" className="inline" />) by MOUSSAID Khalid
+        </p>
       </div>
     </div>
-
-  )
+  );
 }
