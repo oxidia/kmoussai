bb2e9b1282dbaf01c5440136a3763dc35420917f
Khalid Moussaid <khalidmossaid@gmail.com>
2021-09-26 11:57:16 +0100

    re-style about page

--- components/About.js
+++ components/About.js
@@ -1,47 +1,53 @@
-import { RiMenuFill, RiCloseFill, RiGithubFill, RiLinkedinFill, RiTwitterFill, RiHeartFill } from 'react-icons/ri'
-import Card from './Card'
-
-
+import {
+  RiMenuFill,
+  RiCloseFill,
+  RiGithubFill,
+  RiLinkedinFill,
+  RiTwitterFill,
+  RiHeartFill,
+} from "react-icons/ri";
+import Card from "./Card";
 
 export default function About({ title }) {
-
-    return (
-        <Card title={title}>
-            <div className="hidden md:block w-1/4 h-auto">
-                <img loading='lazy' className="rounded-lg shadow-lg antialiased" src="/profile.jpeg" />
+  return (
+    <Card>
+      <div
+        className="flex flex-col-reverse md:grid md:grid-cols-2 bg-cover"
+        style={{ backgroundImage: "url(bg-min.png)" }}
+      >
+        <div className="flex flex-col justify-center p-5 md:p-16">
+          <div className="flex flex-col md:h-full  justify-evenly">
+            <div className="text-lg font-bold font-mono ">
+              <span className="border-b-2 border-dashed pb-2">Helllo, I'm moussaid khalid</span>
             </div>
-            <div className="sm:w-2/3 px-3 flex flex-row flex-wrap">
-                <div className="w-full text-right text-gray-700 font-semibold relative pt-3 md:pt-0 flex justify-between flex-col">
-                    <div>
-                        <div className="text-2xl dark:text-white leading-tight font-mono uppercase">MOUSSAID khalid</div>
-                        <div className="text-normal dark:text-gray-300 hover:text-gray-400">
-                            <span className="border-b border-dashed border-gray-500 pb-1 font-mono">Full Stack Developer</span>
-                        </div>
-                    </div>
-                    <div className="py-3 text-justify font-mono dark:text-white">
-                        <span className="  ">
-                            Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page
-                            avant impress
-                        </span>
-                        <span className=" ">
-                            Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page
-                            avant impress
-                        </span>
-                    </div>
-                    <div className="pt-3 md:pt-0 bottom-0 right-0 flex flex-row justify-end">
-                        <a className="m-3" href="http://github.com/kmoussai" target="_blank">
-                            <RiGithubFill size={'1.9rem'} color={'black'} />
-                        </a>
-                        <a className="m-3" href="https://www.linkedin.com/in/kmoussai/" target="_blank">
-                            <RiLinkedinFill size={'1.9rem'} color={'#0077b5'} />
-                        </a>
-                        <a className="m-3" href="https://twitter.com/k_moussai" target="_blank">
-                            <RiTwitterFill size={'1.9rem'} color={'#08a0e9'} />
-                        </a>
-                    </div>
-                </div>
+            <div className="text-lg font-bold font-mono py-4 md:p-0">
+              <span className="  ">
+                Full-Stack developer who builds applications with high quality
+                especially maintainable code based on structure not only working
+                applications.
+              </span>
             </div>
-        </Card>
-    )
-
+            <div className="w-max p-3 rounded-md bg-gray-300 hover:shadow-lg">
+              <button>
+                <a
+                  href="https://docs.google.com/document/d/1yToXsHoTW1waz9hR2yXlRPtg5A_diw5E/edit?usp=sharing&ouid=116562508892190857698&rtpof=true&sd=true"
+                  target="_blank"
+                  className="font-semibold"
+                >
+                  View Resume
+                </a>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center items-center">
+          <div
+            className="h-96 w-72 bg-cover"
+            style={{ backgroundImage: "url(./profile-min.png)" }}
+          ></div>
+        </div>
+      </div>
+      <div className="sm:w-2/3 px-3 flex flex-row flex-wrap"></div>
+    </Card>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ import WorkExperinece from "../components/WorkExperience";
 
 export default function Home() {
   return (
-    <div className="p-2 min-h-full bg-gray-50 dark:bg-gray-900 sm:border-red-600 border md:border-green-600 lg:border-blue-600 relative">
+    <div className="p-0 min-h-full bg-gray-50 dark:bg-gray-900 sm:border-red-600 border md:border-green-600 lg:border-blue-600 relative">
       <style global jsx>{`
         #__next {
           height: 100%;
@@ -28,19 +28,42 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <header>
-        <div className="flex flex-row justify-end">
+        <div className="pr-2 flex flex-row justify-end ">
+          <div className="pt-0 flex flex-row justify-end">
+            <a
+              className="m-3"
+              href="http://github.com/kmoussai"
+              target="_blank"
+            >
+              <RiGithubFill size={"1.9rem"} color={"black"} />
+            </a>
+            <a
+              className="m-3"
+              href="https://www.linkedin.com/in/kmoussai/"
+              target="_blank"
+            >
+              <RiLinkedinFill size={"1.9rem"} color={"#0077b5"} />
+            </a>
+            <a
+              className="m-3"
+              href="https://twitter.com/k_moussai"
+              target="_blank"
+            >
+              <RiTwitterFill size={"1.9rem"} color={"#08a0e9"} />
+            </a>
+          </div>
           <div className="m-2 p-0 border border-gray-600 bg-gray-600" />
 
-          <div className="md:flex items-center justify-center m-1 cursor-pointer">
+          <div className="flex items-center justify-center m-1 cursor-pointer">
             <Toggle />
           </div>
         </div>
       </header>
-      <div className="mt-2 mb-2 h-1 bg-gray-600 dark:bg-gray-800" />
+      {/* <div className="mt-2 mb-2 h-1 bg-gray-600 dark:bg-gray-800" /> */}
       <div>
-        <div className=" transform bg-blue-500 w-full flex justify-center text-5xl -left-1/4 uppercase">
+        {process.env.NEXT_PUBLIC_SHOW_WIP == 'true' && <div className=" transform bg-blue-500 w-full flex justify-center text-5xl -left-1/4 uppercase">
           work in progress
-        </div>
+        </div>}
       </div>
       <div className="">
         <About title="About me" />
